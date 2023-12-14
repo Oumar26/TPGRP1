0907ebb761bd94ae99d1f44cff9b75b27c307620
Oumar26 <113768204+Oumar26@users.noreply.github.com>
2023-12-14 14:24:57 +0100

    COmmit

---
+++ js/moyen.js
@@ -0,0 +1,110 @@
+//Recuperer toutes les div
+const crds= document.querySelectorAll('.card')
+//randomisation
+//La propriété style.order definit l'ordre de passage des conteneurs
+crds.forEach(card => {
+    let pos= Math.floor(Math.random() * 12);
+    card.style.order= pos;
+});
+let flip= false;//variable booléenne pour la gestion de deux cards, true-->pour le premier click, et false pour le second
+let pause=false;//une variable booléenne qui bloquera les autres cards quand deux sont selectionnés
+let premierClick,deuxiemeClick;
+let gameOver = false;//Pour stopper le jeux lorsqu'il perd
+let gagner= false;//Pour stopper le jeux lorsqu'il gagne
+let carteTrouver=0;
+let nb=8;//nombre de tentatives
+
+const doc= document.getElementById("count");
+doc.innerHTML= "Coups: "+ nb;
+ 
+
+//Boucle foreach pour parcourir cet array et ajouter des events
+crds.forEach(card => card.addEventListener(('click'), effetFlip) );
+
+
+function effetFlip(){
+    if(gameOver || gagner)return ;
+    if(pause) return;//il va bloquer un click sur une troisième card, pendant l'evaluation des deux premières
+    if(this===premierClick) return;//Dans le cas d'un double clique sur la même carte
+    this.classList.add('cliq')//creation d'une classList
+    if(!flip){
+        flip=true//premier clique
+        premierClick= this
+    }else{
+        //deuxieme Clique
+        flip= false;
+        deuxiemeClick= this
+
+        //Matching
+        console.log(premierClick, deuxiemeClick)
+        matchCard()
+        nouvellePartie()
+    }
+
+}
+
+
+
+
+
+function matchCard(){
+    pause=true;
+    if(premierClick.dataset.img === deuxiemeClick.dataset.img){
+        premierClick.removeEventListener('click' , effetFlip)
+        deuxiemeClick.removeEventListener('click', effetFlip)
+        premierClick=null
+        deuxiemeClick=null
+        pause=false
+        flip=false
+        carteTrouver++
+    }else{
+        //Les supprimer de la classList 
+        setTimeout(()=>{
+            premierClick.classList.remove('cliq')
+            deuxiemeClick.classList.remove('cliq')
+
+            flip=false
+            pause=false;
+        }, 1500);
+    }
+    nb--;
+    doc.innerHTML= "Coups: "+ nb
+}
+
+function nouvellePartie(){
+    if(nb==0 && carteTrouver!=4){
+        displayGameOver()
+        gameOver = true
+        }else{
+            gameOver= false
+        }
+
+        if(carteTrouver==4 ||(nb==0 && carteTrouver==6)){
+            displayGagner()
+            gagner= true
+        }else{
+            gagner = false;
+        }
+}
+
+//perdu
+function displayGameOver(){
+document.getElementById("gO").style.display = "block";
+}
+
+
+//gagner
+function displayGagner(){
+    document.getElementById("gagne").style.display = "block";
+}
+
+
+
+function reload(){
+window.location.reload();
+}
+
+function menu(){
+    window.location.href="level.html"
+}
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+## Les règles de ce mémory sont simples :
+**Le jeu est divisé en trois niveaux:**
+- Facile avec 4 paires de cartes : on a droit à 6 tentatives, vous perdez lorsque vous n'arriviez pas à trouver toutes les paires durant ces tentatives, sinon gagnez.
+- Le niveau moyen et difficile obéit à la même règle, mais avec respectivement 6 paires de cartes et 8 tentatives pour le niveau moyen et 8 paires de cartes et 10 tentatives pour le niveau difficile.
+
+
+## Côté blindage de codes :
+- Le blocage des autres cartes quand 2 cartes sont en cours de lecture
+- On ne peut pas non plus sélectionner à la fois 3 cartes
+- Cliquer plus d'une fois sur la même carte sera toujours considéré comme une seule fois
